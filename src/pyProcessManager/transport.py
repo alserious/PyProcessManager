2e6486907f0b2511b38eb5908647bf1fe76f77a7
alserious <_>
2022-07-16 03:10:32 -0700

    add transport

---
+++ src/pyProcessManager/transport.py
@@ -0,0 +1,21 @@
+from paramiko.client import SSHClient
+
+
+class Local:
+    """_summary_"""
+
+    def __init__(self):
+        """_summary_"""
+        pass
+
+
+class OverSSH(SSHClient):
+    """_summary_
+
+    Args:
+        SSHClient (_type_): _description_
+    """
+
+    def __init__(self):
+        """_summary_"""
+        pass
